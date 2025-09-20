e4046b8935a3eb2145c2a83bff3a818e36b6968a
Jyoti <jg.jyotigaikwad121@gmail.com>
2025-09-20 10:04:23 +0530

    Add

---
+++ src/main/java/program50/program1.java
@@ -0,0 +1,13 @@
+package program50;
+
+public class program1 {
+public static void main(String[] args) {
+	// odd or even
+	int num=34;
+	if(num%2==0) {
+		System.out.println("even");
+	}else {
+		System.out.println("odd");
+	}
+}
+}
